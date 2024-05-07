962bea7d1386bd650baad4aa6d8923f1d018b105
Ispánki Dávid Simon <d.ispanki@gmail.com>
2024-05-07 20:17:29 +0200

    v0.5.2 RestAPI: /addRunner OK

--- src/main/java/gde/axihbu/beadando/BeadandoRestAPIController.java
+++ src/main/java/gde/axihbu/beadando/BeadandoRestAPIController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api")
@@ -32,8 +33,8 @@ public class BeadandoRestAPIController {
     }
     
     @PostMapping("/addRunner")
-    public String addRunner() {
-        return "";
+    public VersenyzoEntry addRunner(@RequestBody VersenyzoEntry newRunner) {
+        return versenyzoRepository.save(newRunner);
     }
     
     @GetMapping("/getRaceRunner/{ID}")
