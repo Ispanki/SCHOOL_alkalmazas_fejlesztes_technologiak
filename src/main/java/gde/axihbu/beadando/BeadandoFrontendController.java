7ea64a1c2d84ad6fc70054076c5bf2629caae03c
Ispánki Dávid Simon <d.ispanki@gmail.com>
2024-05-07 19:00:09 +0200

    v0.5.0 New Page: view each race

--- src/main/java/gde/axihbu/beadando/BeadandoFrontendController.java
+++ src/main/java/gde/axihbu/beadando/BeadandoFrontendController.java
@@ -49,12 +49,14 @@ public class BeadandoFrontendController {
     @GetMapping("/verseny/{id}")
     public String eredmenyek(@PathVariable Long id, Model model) {
         try {
-            EredmenyEntry eredmenyek = eredmenyRepository.findById(id).orElseThrow(()-> new RuntimeException("Eredmények nem találhatóak ilyen azonosítóval: " + id));
+            VersenyEntry verseny = versenyRepository.findById(id).orElseThrow(() -> new RuntimeException("Nem található ilyen verseny. Azonosító: " + id));
+            List<EredmenyEntry> eredmenyek = eredmenyRepository.findAllByVerseny_VersenyId(id);
+    
+            model.addAttribute("versenyNev", verseny.getNev());
             model.addAttribute("eredmenyek", eredmenyek);
             return "eredmenyek";
         } catch (Exception e) {
             return "redirect:/verseny";
         }
-    }
-    
+    }    
 }
--- src/main/java/gde/axihbu/beadando/repository/EredmenyRepository.java
+++ src/main/java/gde/axihbu/beadando/repository/EredmenyRepository.java
@@ -1,7 +1,11 @@
 package gde.axihbu.beadando.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gde.axihbu.beadando.entry.EredmenyEntry;
 
-public interface EredmenyRepository extends JpaRepository<EredmenyEntry,Long > {}
+public interface EredmenyRepository extends JpaRepository<EredmenyEntry,Long > {
+    List<EredmenyEntry> findAllByVerseny_VersenyId(Long id);
+}
